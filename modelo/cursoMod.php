8cc1dba57f86200ce35a4cae8f1ad0700ff5029a
Javier <darkmares@gmail.com>
2013-03-18 15:40:52 -0600

    Archivo modelo de la clase Curso

---
+++ modelo/cursoMod.php
@@ -0,0 +1,120 @@
+<?php
+
+class cursoMod{
+
+	/**
+	* @return mixed, array $alumno
+	* 
+	*/	
+
+	private $curso;
+	private $lista_curso;
+
+	//Funcion que regresa todos los registros del curso	
+	function cursoTodo(){
+
+		include('db_data.inc');
+		$conexion = new mysqli($host,$user,$pass,$db);	
+		if($conexion -> connect_errno)
+			die('No hay conexion');
+
+		//Creo mi querry
+		$consulta = "SELECT
+				*
+			     FROM
+				curso";
+
+		//Ejecuto la consulta
+		$result = $conexion -> query($consulta);
+		
+		if($conexion->errno){
+			$conexion -> close();
+			
+			return FALSE;
+		}
+		
+		if(!$result->num_rows > 0)
+			return FALSE;
+
+		//regreso mi objeto de alumno
+		return $result;
+
+		//Procesamos el resultado para convertirlo en un array
+		while ( $fila = $result -> fetch_assoc() )
+			$curso[] = $fila;
+
+		//regreso mi arreglo de alumno
+		return $curso;
+	}
+	
+	function cursoInsert(){
+		$id = $_REQUEST['id'];
+		$nom = $_REQUEST['nom'];
+		$sec = $_REQUEST['sec'];
+		$nrc = $_REQUEST['nrc'];
+		$aca = $_REQUEST['aca'];
+		$dia = $_REQUEST['dia'];
+		$hor = $_REQUEST['hor'];
+		$profid = $_REQUEST['profid'];
+		$cursoid = $_REQUEST['cursoid'];
+
+		//cargo los datos para la conexion
+		include('db_data.inc');		
+		$conexion = new mysqli($host,$user,$pass,$db);	
+		if($conexion -> connect_errno)
+			die('No hay conexion');
+
+		//Creo mi querry
+		$consulta = "INSERT INTO curso VALUES
+			('$id',
+			'$nom',
+			'$sec',
+			'$nrc',
+			'$aca',
+			'$dia',
+			'$hor',
+			'$profid',
+			'$cursoid')";
+
+		//Ejecuto la consulta
+		$result = $conexion -> query($consulta);
+		//var_dump($conexion);
+		if($conexion->errno){
+			$conexion -> close();
+			die('No se pudo establacer la insercion '.$conexion->error);
+		}
+		else
+			echo "1 registro agregado";
+			
+		$conexion -> close();
+		
+	}
+
+	function cursoDelete(){
+		$id = $_REQUEST['id'];
+
+		//cargo los datos para la conexion
+		include('db_data.inc');		
+		$conexion = new mysqli($host,$user,$pass,$db);	
+		if($conexion -> connect_errno)
+			die('No hay conexion');
+
+		//Creo mi querry
+		$consulta = "DELETE FROM curso WHERE idCurso = '$id'";
+
+		//Ejecuto la consulta
+		$result = $conexion -> query($consulta);
+		//var_dump($conexion);
+		if($conexion->errno){
+			$conexion -> close();
+			die('No se pudo establacer el borrado '.$conexion->error);
+		}
+		else
+			echo "registro(s) borrado(s)";
+			
+		$conexion -> close();
+		
+	}
+
+}
+?>
